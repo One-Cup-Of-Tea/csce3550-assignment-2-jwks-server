59c6984c3dece0a3eca6f605bc3d9b226ced7b2d
Shayaan <ShayaanAhmed@my.unt.edu>
2026-09-17 06:53:47 -0500

    Clean up screenshots and update server output

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ const server = createJwksServer();
 
 server.listen(port, () => {
   console.log(`JWKS server listening at http://localhost:${port}`);
+  console.log(`JWKS endpoint: http://localhost:${port}/.well-known/jwks.json`);
 });
